0ae48bb91ea7dbbd976252a8865449a06242d2a9
Elliott Zwertvaegher <elliott.zz59@gmail.com>
2023-10-27 11:11:56 +0900

    Fix base for demo on github pages

--- demo-vite.config.ts
+++ demo-vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ mode }) => ({
+  base: "/vue-useoptimistic/",
   plugins: [vue()],
   esbuild: {
     drop: mode === "production" ? ["console", "debugger"] : [],
